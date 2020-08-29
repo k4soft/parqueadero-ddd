fb889a646d179630ef4f7d757d4f2ce0e6055f29
dgpragmatic <info@k4soft.com>
2020-08-28 19:40:25 -0500

    Se configura el swagger

---
+++ src/main/java/co/com/k4soft/parqueadero/config/Swagger.java
@@ -0,0 +1,42 @@
+package co.com.k4soft.parqueadero.config;
+
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger {
+
+
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("co.com.k4soft.parqueadero"))
+                .paths(PathSelectors.regex("/.*"))
+                .build().apiInfo(apiEndPointsInfo());
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("API de parqueadero sw3")
+                .description("Servicios realizar movimientos del parqueadero")
+                .contact(new Contact("SW 3", "www.k4soft.com", "info@k4soft.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+
+
+    }
+
+}
